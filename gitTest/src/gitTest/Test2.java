c721a883d056612b3685bf9cf3744c6ffc6b568e
YKprince <songkruise@naver.com>
2020-12-25 18:35:17 +0900

    깃 테스트

---
+++ gitTest/src/gitTest/Test2.java
@@ -0,0 +1,19 @@
+package gitTest;
+
+public class Test2{
+
+	public Test2() {
+		// TODO Auto-generated constructor stub
+	}
+	public void goTest(String a, String b) {
+	
+		System.out.println(a+b);
+	}
+	
+	public void goTest2() {
+		
+		goTest("장지웅", "페도");
+	}
+	
+		
+}
