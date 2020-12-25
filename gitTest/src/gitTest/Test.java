73ab6d49b8fa08a2c2a1da146f37585d5180c672
YKprince <songkruise@naver.com>
2020-12-25 18:38:25 +0900

    adsdsa

--- gitTest/src/gitTest/Test.java
+++ gitTest/src/gitTest/Test.java
@@ -6,6 +6,9 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("git Test");
 		System.out.println("yes Test");
+		System.out.println("yes Test");
+		System.out.println("yes Test");
+		System.out.println("yes Test");
 	}
 
 }
